4060e39a68bfc36631d8acdca7d2cca1713f4b5c
RobielFvaj <robiel.tesfazghi.eclat@fvaj.ch>
2020-10-09 10:45:20 +0200

    Revert "React travel form"
    This reverts commit caf5f30119b9bb86618fef778c85a47c11863739.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import React from 'react';
+import TodoItem from './TodoItem'
+import todosData from './todosData';
 
-class App extends React.Component {
+class App extends React.Component{
  
   constructor(){
-    super()
-    this.state = {
-     
+    super();
+    this.state ={
+      todos : todosData
     }
-   
+    this.handleChange = this.handleChange.bind(this)
   }
 
- render(){
-    return (
-     <div>
-       
-     </div>
-    )
+  handleChange(id){
+    this.setState(prevState=> {
+      const updatedTodos = prevState.todos.map(todo => {
+        if(todo.id === id){
+          return{...todo,
+            completed: !todo.completed
+          }
+        
+      }
+        return todo
+      })
+      return {
+        todos:updatedTodos}
+      })
   }
+  
+  render() {
+    const todoItems = this.state.todos.map(item =>
+    <TodoItem 
+      item= {item}
+      key = {item.id}
+      handleChange = {this.handleChange}
+    />)
+
+
+    return (
+      <div className="todo-list">
+        {todoItems}
+      </div>
+    )
+}
 }
 
 export default App;
